ceaa6da10d7577a29e67b58be528ca14e6d612c7
ヾ(≧∇≦*)ゝ <krisdanoda@users.noreply.github.com>
2023-01-17 13:17:05 +0100

    Update ShowFacadeTest.java

--- src/test/java/facades/ShowFacadeTest.java
+++ src/test/java/facades/ShowFacadeTest.java
@@ -4,11 +4,15 @@ import dtos.ShowDto;
 import entities.Show;
 import utils.EMF_Creator;
 import entities.RenameMe;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -20,6 +24,8 @@ public class ShowFacadeTest {
     private static EntityManagerFactory emf;
     private static ShowFacade facade;
 
+    public static Show testShow1, testShow2;
+
     public ShowFacadeTest() {
     }
 
@@ -27,6 +33,20 @@ public class ShowFacadeTest {
     public static void setUpClass() {
         emf = EMF_Creator.createEntityManagerFactoryForTest();
         facade = ShowFacade.getFacade(emf);
+        EntityManager em = emf.createEntityManager();
+
+         testShow1 = new Show("TestShow1", 300, "tomorrow", "denmark");
+         testShow2 = new Show("TestShow2", 5000, "DayAfterTomorrow", "japan");
+
+        try {
+            em.getTransaction().begin();
+            em.persist(testShow1);
+            em.persist(testShow2);
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+
     }
 
     @AfterAll
@@ -61,8 +81,24 @@ public class ShowFacadeTest {
         ShowDto showDto = facade.createShow(new ShowDto(showExpected));
         Show acutal = showDto.creatEntity();
 
-        assertEquals(showExpected,acutal);
-        assert (acutal.getId()!=0);
+        assertEquals(showExpected, acutal);
+        assert (acutal.getId() != 0);
+
+    }
+
+    @Test void getAllShows(){
+        assertEquals(2, facade.getShows().size());
+    }
+
+    @Test void updateShow(){
+
+        testShow1.setName("differentName");
+        testShow1.setDuration(testShow1.getDuration() + 50000);
+        testShow1.setLocation("not in denmark");
+
+        ShowDto actualDTO =facade.updateShow(new ShowDto(testShow1));
+
+        assertEquals(testShow1, actualDTO.creatEntity());
 
     }
 
